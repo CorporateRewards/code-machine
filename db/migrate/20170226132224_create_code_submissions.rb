a304b221e81461a00d1df56b4ab5dd0fc7540e78
Gillian Manning <gillian.manning@corporaterewards.co.uk>
2017-03-25 12:41:42 +0000

    remove old files

--- db/migrate/20170226132224_create_code_submissions.rb
+++ db/migrate/20170226132224_create_code_submissions.rb
@@ -3,8 +3,7 @@ class CreateCodeSubmissions < ActiveRecord::Migration[5.0]
     create_table :code_submissions do |t|
       t.string :code
       t.integer :user_id	
-      t.string :user_email
-      
+      t.string :user_email      
       t.timestamps
     end
   end
